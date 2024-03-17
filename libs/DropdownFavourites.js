22118922f994b89a6832a23d7eca82d689fb6f58
Ben Arthur <76140109+BenArthurOce@users.noreply.github.com>
2024-03-17 18:28:45 +1100

    Delete libs/DropdownFavourites.js

--- libs/DropdownFavourites.js
+++
@@ -1,9 +0,0 @@
-// Favourites.js
-const dropdownFavourites = [
-    "1.e4 e5 2.Nf3 Nc6 3.d4",
-    "1.d4 e5",
-    "1.e4 e5 2.Nc3",
-    "1.e4 e5 2.Nf3 Nc6 3.c3 d5 4.Bb5 dxe4 5.Nxe5 Qd5 6.Qa4",
-];
-
-export default dropdownFavourites;
